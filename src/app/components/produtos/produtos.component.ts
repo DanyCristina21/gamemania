323082771404c4ec3b601a105b20f783416482c5
Daniela Cristina <danielacristina2101@outlook.com>
2024-11-30 10:45:47 -0300

    Atualização do Filtro por Categorias

--- src/app/components/produtos/produtos.component.ts
+++ src/app/components/produtos/produtos.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-produtos',
-  imports: [],
+  imports: [ NgFor ],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
-export class ProdutosComponent {
+export class ProdutosComponent implements OnInit {
+
+  categories: string[] = ['Games', 'Hardware', 'Work', 'Periféricos', 'Smartphones'];
+  selectedCategory: string = 'Games'; // Categoria inicial
+
+  products: any[] = [
+    { id: 1, name: 'Red Dead Redemption II para PS4', category: 'Games', price: 125, imageUrl: 'assets/products/jogo.png' },
+    { id: 2, name: 'Memória RAM Corsair, 4GB, 1333MHz, DDR3, CL9', category: 'Hardware', price: 99, imageUrl: 'assets/products/memoria.png' },
+    { id: 3, name: 'Cadeira Mymax Presidente Mesh Giratoria', category: 'Work', price: 280, imageUrl: 'assets/products/cadeira.png' },
+    { id: 4, name: 'Notebook Acer Aspire 5 Intel Core i5, 8GB RAM 512GB SSD, Tela 15,6 Full HD', category: 'Work', price: 2499, imageUrl: 'assets/products/notebook.png' },
+    { id: 5, name: 'Headphone Gamer Brazilpc Bpc-m18 RGB C/ Microfone RGB Box', category: 'Periféricos', price: 120, imageUrl: 'assets/products/fone.png' },
+    { id: 6, name: 'Smartphone Motorola Moto G04s, 8GB RAM, 128GBt', category: 'Smartphones', price: 720, imageUrl: 'assets/products/celular.png' },
+  ];
+
+  filteredProducts: any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.filterProductsByCategory();
+  }
+
+  // Filtra os produtos com base na categoria selecionada
+  filterProductsByCategory() {
+    this.filteredProducts = this.products.filter(product => product.category === this.selectedCategory);
+  }
+
+  // Função para alterar a categoria selecionada
+  onCategoryChange(category: string) {
+    this.selectedCategory = category;
+    this.filterProductsByCategory();
+  }
 
 }
