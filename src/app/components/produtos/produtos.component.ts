6b8317ffd7ecee91ef8306ef8018f85e64755399
Daniela Cristina <danielacristina2101@outlook.com>
2024-11-30 14:46:17 -0300

    Correção da busca do Filtro por Categorias

--- src/app/components/produtos/produtos.component.ts
+++ src/app/components/produtos/produtos.component.ts
@@ -13,12 +13,12 @@ export class ProdutosComponent implements OnInit {
   selectedCategory: string = 'Games'; // Categoria inicial
 
   products: any[] = [
-    { id: 1, name: 'Red Dead Redemption II para PS4', category: 'Games', price: 125, imageUrl: 'assets/products/jogo.png' },
-    { id: 2, name: 'Memória RAM Corsair, 4GB, 1333MHz, DDR3, CL9', category: 'Hardware', price: 99, imageUrl: 'assets/products/memoria.png' },
-    { id: 3, name: 'Cadeira Mymax Presidente Mesh Giratoria', category: 'Work', price: 280, imageUrl: 'assets/products/cadeira.png' },
-    { id: 4, name: 'Notebook Acer Aspire 5 Intel Core i5, 8GB RAM 512GB SSD, Tela 15,6 Full HD', category: 'Work', price: 2499, imageUrl: 'assets/products/notebook.png' },
-    { id: 5, name: 'Headphone Gamer Brazilpc Bpc-m18 RGB C/ Microfone RGB Box', category: 'Periféricos', price: 120, imageUrl: 'assets/products/fone.png' },
-    { id: 6, name: 'Smartphone Motorola Moto G04s, 8GB RAM, 128GBt', category: 'Smartphones', price: 720, imageUrl: 'assets/products/celular.png' },
+    { id: 1, name: 'Red Dead Redemption II para PS4', category: 'GAMES - Os melhores jogos do mercado', price: 125, imageUrl: 'assets/products/jogo.png' },
+    { id: 2, name: 'Memória RAM Corsair, 4GB, 1333MHz, DDR3, CL9', category: 'HARDWARE - As melhores ofertas de hardware estão aqui', price: 99, imageUrl: 'assets/products/memoria.png' },
+    { id: 3, name: 'Cadeira Mymax Presidente Mesh Giratoria', category: 'WORK - tudo o que você precisa para o seu home office', price: 280, imageUrl: 'assets/products/cadeira.png' },
+    { id: 4, name: 'Notebook Acer Aspire 5 Intel Core i5, 8GB RAM 512GB SSD, Tela 15,6 Full HD', category: 'WORK - tudo o que você precisa para o seu home office', price: 2499, imageUrl: 'assets/products/notebook.png' },
+    { id: 5, name: 'Headphone Gamer Brazilpc Bpc-m18 RGB C/ Microfone RGB Box', category: 'PERIFÉRICOS - aqui você encontra todos os dispositivos que necessita', price: 120, imageUrl: 'assets/products/fone.png' },
+    { id: 6, name: 'Smartphone Motorola Moto G04s, 8GB RAM, 128GBt', category: 'SMARTPHONES - celulares para todos gostos e bolsos', price: 720, imageUrl: 'assets/products/celular.png' },
   ];
 
   filteredProducts: any[] = [];
