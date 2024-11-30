90e7f9825bcdfb5c274c2300f314ac987a1720f9
Daniela Cristina <danielacristina2101@outlook.com>
2024-11-30 14:17:37 -0300

    Ajustes no menu (navbar) e na barra de pesquisa (searchbar)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
