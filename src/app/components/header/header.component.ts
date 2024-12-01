41f4637c167f0eef1800e9f8d9c1434776ac4fa0
Daniela Cristina <danielacristina2101@outlook.com>
2024-11-30 22:03:09 -0300

    Correção do cabeçalho (header)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { SearchbarComponent } from '../searchbar/searchbar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [SearchbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  searchTerm: string = '';
+
+  onSearch(): void {
+    if (this.searchTerm.trim()) {
+      console.log(`Pesquisando por: ${this.searchTerm}`);
+      // Aqui você pode chamar um serviço ou filtrar dados com base no searchTerm.
+    } else {
+      console.log('Campo de pesquisa vazio!');
+    }
+  }
 
 }
